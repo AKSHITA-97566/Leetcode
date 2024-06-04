449d8f386eb979a344999005670e2cc235c0584d
AKSHITA-97566 <129294738+AKSHITA-97566@users.noreply.github.com>
2024-06-04 11:32:26 +0530

    Time: 0 ms (100%), Space: 40.3 MB (46.98%) - LeetHub

---
+++ 2507-number-of-common-factors/2507-number-of-common-factors.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int n=gcd(a,b);
+        int count=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(a%i==0 && b%i==0)
+            {
+                count++;
+            }
+
+        }
+        return count;
+
+    }
+    public static int gcd(int a, int b)
+    {
+        if(b==0)
+        {
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+}
